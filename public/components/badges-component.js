56030203056c05ab476a039241e20bfb9a182ac8
rbuXtron <101179000+rbuXtron@users.noreply.github.com>
2025-06-11 08:42:51 +0200

    images_sgSaentisBadge_01

--- public/components/badges-component.js
+++ public/components/badges-component.js
@@ -66,7 +66,8 @@ export function renderBadgeRanking(pilots, containerId = 'badge-ranking-containe
     const header = document.createElement('div');
     header.className = 'ranking-header';
     header.innerHTML = `
-        <h2 class="section-title">🏅 WeGlide Badges Saison 2024/2025</h2>
+        <h2 class="section-title"><img src="./images/weglide-badge-logo.png" alt="WeGlide Badge" class="section-logo" 
+     style="width: 32px; height: 32px; margin-right: 8px; vertical-align: middle;"> WeGlide Badges Saison 2024/2025</h2>
         <div class="ranking-subtitle">
             Gesammelte Abzeichen seit Oktober 2024
         </div>
